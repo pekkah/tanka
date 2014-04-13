8a5b15ddd3bfb6b089970f4839a04172206b4456
Pekka Heikura <pekkah@gmail.com>
2014-04-13 22:25:26 +0300

    Bumb version number

--- src/Web/Properties/AssemblyInfo.cs
+++ src/Web/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Web")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright ©  2013-2014 Pekka Heikura")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0")]
-[assembly: AssemblyInformationalVersion("0.1.1")]
+[assembly: AssemblyInformationalVersion("0.1.2")]
