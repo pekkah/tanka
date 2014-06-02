a4902e74cc2daf41a56a47967859bd669b8619e3
Pekka Heikura <pekkah@gmail.com>
2014-06-02 21:47:20 +0300

    Update bundler

--- src/Web/Properties/AssemblyInfo.cs
+++ src/Web/Properties/AssemblyInfo.cs
@@ -1,10 +1,10 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+
 [assembly: AssemblyTitle("Web")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -17,9 +17,11 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
+
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
+
 [assembly: Guid("a6e5a3fd-e1e5-4041-afed-5fe1480ef6a8")]
 
 // Version information for an assembly consists of the following four values:
@@ -33,4 +35,4 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0")]
-[assembly: AssemblyInformationalVersion("0.1.2")]
+[assembly: AssemblyInformationalVersion("0.1.3")]
